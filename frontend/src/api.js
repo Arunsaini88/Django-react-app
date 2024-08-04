6770da879bb62d2838860d7af588b1898b428730
Arun <as9389872806@gmail.com>
2024-08-04 20:45:37 +0530

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,22 +1,24 @@
-import axios from "axios"
-import { ACCESS_TOKEN } from "./constants"
+import axios from "axios";
+import { ACCESS_TOKEN } from "./constants";
 
-const api= axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+const apiUrl = "/choreo-apis/djangoreactapp/backend/v1";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+});
 
 api.interceptors.request.use(
-    (confirm) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
+  (confirm) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
 
-        if(token) {
-            confirm.headers.Authorization=`Bearer ${token}` 
-        }
-        return confirm
-    },
-    (error) => {
-        return Promise.reject(error)
+    if (token) {
+      confirm.headers.Authorization = `Bearer ${token}`;
     }
-)
+    return confirm;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-export default api
+export default api;
